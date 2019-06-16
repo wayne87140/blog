081ea79f36d7ae34112f3b850eabfd85b4c4efee
wayne87140 <wayne87140@gmail.com>
2019-06-16 16:48:50 +0800

    ch20 0616

--- blog/article/urls.py
+++ blog/article/urls.py
@@ -4,4 +4,6 @@ from article import views
 app_name = 'article'
 urlpatterns = [
     path('', views.article, name='article'),
+    path('articleCreate/', views.articleCreate, name='articleCreate'),
+    path('articleRead/<int:articleId>/', views.articleRead, name='articleRead'),
 ]
